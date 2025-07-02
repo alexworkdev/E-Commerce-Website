128902799a4e950890ef9560e4412eb7b86f5509
alexernandes10041-debugger <alexernandes10041@gmail.com>
2025-07-02 19:28:49 +0530

    Project upload

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(cors({
   origin: [
-    'https://e-commerce-website-7e16rchmp-suman-choudhurys-projects.vercel.app',
+    'https://e-commerce-website-orcin-xi.vercel.app',
     'http://localhost:3000'
   ],
   credentials: true
